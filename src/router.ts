133f77f8f0c163dd5b69f0a12feb133897861702
TassianaSgarbi <tassiana.sgarbi@gmail.com>
2024-11-10 17:59:05 -0300

    Alteração Rota Login para post

--- src/router.ts
+++ src/router.ts
@@ -16,7 +16,7 @@ const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 
-router.get('/session', new AuthUserController().handle)
+router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
