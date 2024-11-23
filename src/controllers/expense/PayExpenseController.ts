866a8152439863a347272a4a382b3d10d0ee135c
TassianaSgarbi <tassiana.sgarbi@gmail.com>
2024-11-23 09:45:47 -0300

    Pay Expense

--- src/controllers/expense/PayExpenseController.ts
+++ src/controllers/expense/PayExpenseController.ts
@@ -18,17 +18,10 @@ class PayExpenseController{
 
         const expense_id = req.query.expense_id as string
 
-        const { new_status } = req.body
-
-        console.log(req.body);
-        
-        console.log(new_status);
-
         const payExpense = new PayExpenseService()
         
         const paidExpense = await payExpense.execute({
             expense_id,
-            new_status,
             user_id
         })
 
